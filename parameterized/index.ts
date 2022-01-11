21b287e73ace3ee2b8924f1cb155af75189eae58
Haseeb Ahmad <ahmad.has03@gmail.com>
2022-01-10 20:56:52 -0500

    updated exports

--- parameterized/index.ts
+++ parameterized/index.ts
@@ -1,9 +1,5 @@
 export * from './query-builder';
 
-export { default as constants } from './constants';
 export * as types from './types';
-export * as exceptions from './exceptions';
 export * as predicates from './predicate';
 export * as statements from './statements';
-
-export * as parameterized from './parameterized';
---
+++ query-builder/index.ts
@@ -0,0 +1,6 @@
+export { default as BaseQuery } from './BaseQuery';
+export { default as QueryBuilder } from './QueryBuilder';
+export { default as Select } from './Select';
+export { default as Insert } from './Insert';
+export { default as Update } from './Update';
+export { default as Delete } from './Delete';
